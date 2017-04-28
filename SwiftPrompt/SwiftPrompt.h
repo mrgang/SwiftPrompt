0347a243db6239d07324b3b8eeb17868f682f408
iosdeveloper <li93gang@163.com>
2017-04-28 11:07:27 +0800

    Initial Commit

---
+++ SwiftPrompt/SwiftPrompt.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPrompt.h
+//  SwiftPrompt
+//
+//  Created by iOS Developer on 2017/4/28.
+//  Copyright © 2017年 mrgang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPrompt.
+FOUNDATION_EXPORT double SwiftPromptVersionNumber;
+
+//! Project version string for SwiftPrompt.
+FOUNDATION_EXPORT const unsigned char SwiftPromptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPrompt/PublicHeader.h>
+
+
